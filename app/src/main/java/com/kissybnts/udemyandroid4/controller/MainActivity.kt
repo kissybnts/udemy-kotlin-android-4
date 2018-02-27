15e25619c77b746c5b8fa949a39c95397296dba1
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-02-27 21:17:00 +0900

    Lecture 60, Simple ListView

--- app/src/main/java/com/kissybnts/udemyandroid4/controller/MainActivity.kt
+++ app/src/main/java/com/kissybnts/udemyandroid4/controller/MainActivity.kt
@@ -2,12 +2,21 @@ package com.kissybnts.udemyandroid4.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.kissybnts.udemyandroid4.R
+import com.kissybnts.udemyandroid4.model.Category
+import com.kissybnts.udemyandroid4.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
