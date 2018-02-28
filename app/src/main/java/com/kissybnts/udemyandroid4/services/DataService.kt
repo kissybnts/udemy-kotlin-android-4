d4e3b88c0265a4924d0d5f2bd13c8ae305b337dc
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2018-02-28 21:17:24 +0900

    Lecture 65, Product List View

--- app/src/main/java/com/kissybnts/udemyandroid4/services/DataService.kt
+++ app/src/main/java/com/kissybnts/udemyandroid4/services/DataService.kt
@@ -11,25 +11,36 @@ object DataService {
             Category("DIGITAL", "digitalgoodsimage")
     )
 
-    val hats = listOf(
+    private val hats = listOf(
             Product("Hat1", "$18", "hat1"),
             Product("Hat2", "$26", "hat2"),
             Product("Hat3", "$10", "hat3"),
             Product("Hat4", "$199", "hat4")
     )
 
-    val hoodies = listOf(
+    private val hoodies = listOf(
             Product("Hoodies 1", "$50", "hoodie1"),
             Product("Hoodies 2", "$44", "hoodie2"),
             Product("Hoodies 3", "$56", "hoodie3"),
             Product("Hoodies 4", "$59", "hoodie4")
     )
 
-    val shirts = listOf(
+    private val shirts = listOf(
             Product("Shirts 1", "$10", "shirt1"),
             Product("Shirts 2", "$11", "shirt2"),
             Product("Shirts 3", "$12", "shirt3"),
             Product("Shirts 4", "$13", "shirt4"),
             Product("Shirts 5", "$14", "shirt5")
     )
+
+    val digitalGoods: List<Product> = listOf()
+
+    fun getProducts(categoryType: String): List<Product> {
+        return when (categoryType) {
+            "SHIRTS" -> shirts
+            "HOODIES" -> hoodies
+            "HATS" -> hats
+            else -> digitalGoods
+        }
+    }
 }
